15633565da6d13c173f075394b34624083ae858a
tranducduy0304 <54491223+tranducduy0304@users.noreply.github.com>
2023-04-16 21:56:31 +0700

    complete

--- src/App.js
+++ src/App.js
@@ -1,10 +1,66 @@
 import "./App.css";
 import { useGlobalContext } from "./components/context";
+import SetupForm from "./components/SetUpForm";
 import Loading from "./components/Loading";
 import Modal from "./components/Modal";
 
 function App() {
-    return <div className="App"></div>;
+    const {
+        waiting,
+        loading,
+        questions,
+        index,
+        correct,
+        nextQuestion,
+        checkAnswer,
+    } = useGlobalContext();
+    if (waiting) {
+        return <SetupForm />;
+    }
+    if (loading) {
+        return <Loading />;
+    }
+
+    const { question, incorrect_answers, correct_answer } = questions[index];
+    // const answers = [...incorrect_answers, correct_answer]
+    let answers = [...incorrect_answers];
+    const tempIndex = Math.floor(Math.random() * 4);
+    if (tempIndex === 3) {
+        answers.push(correct_answer);
+    } else {
+        answers.push(answers[tempIndex]);
+        answers[tempIndex] = correct_answer;
+    }
+    return (
+        <main>
+            <Modal />
+            <section className="quiz">
+                <p className="correct-answers">
+                    correct answers : {correct}/{index}
+                </p>
+                <article className="container">
+                    <h2 dangerouslySetInnerHTML={{ __html: question }} />
+                    <div className="btn-container">
+                        {answers.map((answer, index) => {
+                            return (
+                                <button
+                                    key={index}
+                                    className="answer-btn"
+                                    onClick={() =>
+                                        checkAnswer(correct_answer === answer)
+                                    }
+                                    dangerouslySetInnerHTML={{ __html: answer }}
+                                />
+                            );
+                        })}
+                    </div>
+                </article>
+                <button className="next-question" onClick={nextQuestion}>
+                    next question
+                </button>
+            </section>
+        </main>
+    );
 }
 
 export default App;
